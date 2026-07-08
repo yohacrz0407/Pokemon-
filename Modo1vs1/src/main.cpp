ec0944c5059588ed2883870768e7eba33dc581f2
Yohalmo Daniel Cruz Peréz <00033626@uca.edu.sv>
2026-07-07 23:26:19 -0600

    arreglando error al compilar

--- Modo1vs1/src/main.cpp
+++ Modo1vs1/src/main.cpp
@@ -5,3 +5,28 @@
 #include "../include/combate.h"
 
 using namespace std;
+
+int main()
+{
+    int opcion;
+
+    do
+    {
+        opcion = menuPrincipal();
+
+        switch (opcion)
+        {
+        case 1:
+            seleccionarEquipos();
+            combate();
+            break;
+
+        case 2:
+            cout << "\nGracias por jugar\n";
+            break;
+        }
+
+    } while (opcion != 2);
+
+    return 0;
+}
